51d65f062d9a65158e71e8206f5b632718288098
hyeyeonie <shy020916@naver.com>
2025-05-09 13:46:00 +0900

    feat: 닉네임 리스트 조회 기능 구현
    - InfoViewController에서 닉네임 검색 UI 구성 및 입력값 처리
    - GetInfoService를 통해 닉네임 리스트 GET 요청 수행
    - 조회 결과를 infoLabel에 출력, 실패 시 에러 메시지 표시
    - UITextField 편집 이벤트 처리 및 검색 버튼 액션 연결

--- practice_week4_Network/practice_week4_Network/View/LoginViewController.swift
+++ practice_week4_Network/practice_week4_Network/View/LoginViewController.swift
@@ -12,20 +12,23 @@ import Then
 
 class LoginViewController: UIViewController {
     
+    // MARK: - Properties
+    
     private var loginId: String = ""
     private var password: String = ""
     private var nickName: String = ""
+    
+    // MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setLayout()
     }
     
-    //    @objc private func infoViewButtonTap() {
-    //        let infoVC = InfoViewController()
-    //        self.present(infoVC, animated: true)
-    //    }
-    
+    @objc private func infoViewButtonTap() {
+        let infoVC = InfoViewController()
+        self.present(infoVC, animated: true)
+    }
     
     @objc private func textFieldDidEditing(_ textField: UITextField) {
         switch textField {
@@ -38,72 +41,7 @@ class LoginViewController: UIViewController {
         }
     }
     
-    private func setLayout() {
-        self.view.backgroundColor = .white
-        self.view.addSubview(stackView)
-        
-        stackView.snp.makeConstraints {
-            $0.leading.trailing.equalTo(self.view.safeAreaLayoutGuide).inset(40)
-            $0.top.bottom.equalTo(self.view.safeAreaLayoutGuide).inset(200)
-        }
-        
-        [idTextField, passwordTextField, nickNameTextField, registerButton, infoViewButton].forEach {
-            self.stackView.addArrangedSubview($0)
-        }
-    }
-    
-    private let stackView = UIStackView().then {
-        $0.axis = .vertical
-        $0.distribution = .equalSpacing
-        $0.spacing = 20
-    }
-    
-    private lazy var idTextField = UITextField().then {
-        $0.addTarget(self,
-                     action: #selector(textFieldDidEditing(_:)),
-                     for: .allEvents)
-        $0.backgroundColor = .lightGray
-        $0.placeholder = "아이디를 입력하슈"
-        $0.layer.cornerRadius = 6
-    }
-    
-    private lazy var passwordTextField = UITextField().then {
-        $0.addTarget(self,
-                     action: #selector(textFieldDidEditing(_:)),
-                     for: .allEvents)
-        $0.backgroundColor = .lightGray
-        $0.placeholder = "패스워드를 입력하슈"
-        $0.layer.cornerRadius = 6
-    }
-    
-    private lazy var nickNameTextField = UITextField().then {
-        $0.addTarget(self,
-                     action: #selector(textFieldDidEditing(_:)),
-                     for: .allEvents)
-        $0.backgroundColor = .lightGray
-        $0.placeholder = "닉네임을 입력하슈"
-        $0.layer.cornerRadius = 6
-    }
-    
-    private lazy var registerButton = UIButton().then {
-        $0.addTarget(self,
-                     action: #selector(registerButtonTap),
-                     for: .touchUpInside)
-        $0.backgroundColor = .systemBlue
-        $0.setTitle("회원가입", for: .normal)
-        $0.titleLabel?.textColor = .white
-        $0.layer.cornerRadius = 6
-    }
-    
-    private lazy var infoViewButton = UIButton().then {
-        //        $0.addTarget(self,
-        //                     action: #selector(infoViewButtonTap),
-        //                     for: .touchUpInside)
-        $0.backgroundColor = .systemBlue
-        $0.setTitle("회원정보 조회", for: .normal)
-        $0.titleLabel?.textColor = .white
-        $0.layer.cornerRadius = 6
-    }
+    // MARK: - Actions
     
     @objc public func registerButtonTap() {
         Task {
@@ -134,5 +72,78 @@ class LoginViewController: UIViewController {
             }
         }
     }
+    
+    // MARK: - Layout
+    
+    private func setLayout() {
+        self.view.backgroundColor = .white
+        self.view.addSubview(stackView)
+        
+        stackView.snp.makeConstraints {
+            $0.leading.trailing.equalTo(self.view.safeAreaLayoutGuide).inset(40)
+            $0.top.bottom.equalTo(self.view.safeAreaLayoutGuide).inset(200)
+        }
+        
+        [idTextField, passwordTextField, nickNameTextField, registerButton, infoViewButton].forEach {
+            self.stackView.addArrangedSubview($0)
+        }
+    }
+    
+    // MARK: - UI Components
+    
+    private let stackView = UIStackView().then {
+        $0.axis = .vertical
+        $0.distribution = .equalSpacing
+        $0.spacing = 20
+    }
+    
+    private lazy var idTextField = UITextField().then {
+        $0.addTarget(self,
+                     action: #selector(textFieldDidEditing(_:)),
+                     for: .allEvents)
+        $0.backgroundColor = .lightGray
+        $0.placeholder = "아이디를 입력하슈"
+        $0.setLeftPadding(10)
+        $0.layer.cornerRadius = 6
+    }
+    
+    private lazy var passwordTextField = UITextField().then {
+        $0.addTarget(self,
+                     action: #selector(textFieldDidEditing(_:)),
+                     for: .allEvents)
+        $0.backgroundColor = .lightGray
+        $0.placeholder = "패스워드를 입력하슈"
+        $0.setLeftPadding(10)
+        $0.layer.cornerRadius = 6
+    }
+    
+    private lazy var nickNameTextField = UITextField().then {
+        $0.addTarget(self,
+                     action: #selector(textFieldDidEditing(_:)),
+                     for: .allEvents)
+        $0.backgroundColor = .lightGray
+        $0.placeholder = "닉네임을 입력하슈"
+        $0.setLeftPadding(10)
+        $0.layer.cornerRadius = 6
+    }
+    
+    private lazy var registerButton = UIButton().then {
+        $0.addTarget(self,
+                     action: #selector(registerButtonTap),
+                     for: .touchUpInside)
+        $0.backgroundColor = .systemBlue
+        $0.setTitle("회원가입", for: .normal)
+        $0.titleLabel?.textColor = .white
+        $0.layer.cornerRadius = 6
+    }
+    
+    private lazy var infoViewButton = UIButton().then {
+        $0.addTarget(self,
+                     action: #selector(infoViewButtonTap),
+                     for: .touchUpInside)
+        $0.backgroundColor = .systemBlue
+        $0.setTitle("회원정보 조회", for: .normal)
+        $0.titleLabel?.textColor = .white
+        $0.layer.cornerRadius = 6
+    }
 }
-
