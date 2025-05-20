05766d11ff97b03fa407c830937e881746d662b5
hyeyeonie <shy020916@naver.com>
2025-05-21 00:38:59 +0900

    [add] #10 SwiftUI overlay modifier 적용 실습

--- week6_practice_SwiftUI/week6_practice_SwiftUI/week6_practice_SwiftUIApp.swift
+++ week6_practice_SwiftUI/week6_practice_SwiftUI/week6_practice_SwiftUIApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct week6_practice_SwiftUIApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ProfileViewWithOverlay()
         }
     }
 }
